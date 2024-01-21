18b5a2a0628883a671890cbaec51f6a2d520f66c
YasinSabir1312 <149936707+YasinSabir1312@users.noreply.github.com>
2024-01-21 15:51:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-song = "";
+song = "music.mp3";
 
 function preload()
 {
@@ -47,8 +47,59 @@ function gotPoses(results)
 
        
     }
+}
 
+function draw() {
+    Image(video, 0, 0, 600, 500);
 
+    Fill("#FF0000");
+    stroke("FF0000");
+
+    if(scoreRightWrist > 0.2)
+
+    Circle(rightWristX, rightWristY, 20);
+
+    if(rightWristY >0 && rightWristY <= 100) {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0,5);
+    }
+    else if(rightWristY >100 && rightWristY <= 200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY >200 && rightWristY <= 300) 
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1,5);
+    }
+    else if(rightWristY >300 && rightWristY <= 400) 
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY >400) 
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2,5);
+    }
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        InNumberLeftWristY = Number(leftWristY);
+        new_leftWristY = floor(InNumberLeftWristY *2);
+        leftWristY_divide_1000 = new_leftWristY/1000;
+        document.getElementById("volume").innerHTML = "Volume = " + leftWristY_divide_1000
+        song.setvolume(leftWristY_divide_1000)
+    }
+        
+    function play()
+    {
+        song.play();
+        song.setVolume(1);
+        song.rate(1);
+    }
 }
 
 
